5a960433be6973657b6f5082252ee78e6e151cad
boufrouk <badreddineoifroukhi20@gmail.com>
2023-10-04 18:35:05 +0200

    test

--- src/AccesBD/ConnectionBD.java
+++ src/AccesBD/ConnectionBD.java
@@ -3,7 +3,7 @@ package AccesBD;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-d
+
 public class ConnectionBD {
     static Connection con=null;
     public ConnectionBD() {
